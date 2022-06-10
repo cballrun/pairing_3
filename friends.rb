669ee0bbadca2c9a29e3a1404232737cf6711134
Carter Ball <carter.k.ball@gmail.com>
2022-06-10 09:50:18 -0400

    Second push

--- friends.rb
+++ friends.rb
@@ -1,6 +1,12 @@
 friends = [10, 20, 30, 40, 50, 60, 70, 80]
-friends.each do |hi|
-  if hi>30
-    p hi
+# friends.each do |hi|
+#   if hi>30
+#     p hi
+#   end
+# end
+
+friends.each do |low|
+  if low<60
+    p low
   end
-end 
+end
